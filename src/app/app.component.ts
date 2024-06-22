eec7bf6125659c61de0e16ea6f04aa8ef98b9fd1
Sebastian Kubrycz <38853186+Reishi006@users.noreply.github.com>
2024-06-22 12:02:00 +0200

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  list:string[] = [];
+
+  addToList(input: string) {
+    console.log(input);
+    this.list.push(input);
+  }
+}
